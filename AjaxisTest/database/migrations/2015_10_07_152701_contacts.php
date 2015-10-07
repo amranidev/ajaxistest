508149514680a42f762e156df9fb3c170f6f44b7
Amrani <amranidev@gmail.com>
2015-10-07 16:57:48 +0100

    ajaxistest

---
+++ AjaxisTest/database/migrations/2015_10_07_152701_contacts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Contacts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts',function (Blueprint $table){
+          $table->increments('id');
+          $table->String('firstname');
+          $table->String('lastname');
+          $table->Date('date');
+          $table->String('phone');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
+}
