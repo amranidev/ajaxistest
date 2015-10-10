ea26af28f65bf6c3d83a3cbc79336d2860d5f2bf
Amrani <amranidev@gmail.com>
2015-10-10 16:35:31 +0100

    index blade configuration

--- AjaxisTest/resources/views/index.blade.php
+++ AjaxisTest/resources/views/index.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.MasterMt')
 @section('title','Ajaxis')
 	<div class = 'container'>
-     <h1><i>Chose between</i></h1>
-     <a href = '/ContactBt/' class = 'btn'>Bootstrap</a>
-	 <a href = '/ContactMt/' class = 'btn'>Materialize</a>
+     <h1><i>Try Ajaxis</i></h1>
+     <a href = '/ContactBt/' class = 'btn waves-effect waves-light #311b92 deep-purple darken-4'>Bootstrap</a>
+	 <a href = '/ContactMt/' class = 'btn waves-effect waves-light red'>Materialize</a>
+	 <br><br>
 	</div>
+	<footer class="page-footer blue">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact</h5>
+                <p class="grey-text text-lighten-4"><i class = 'material-icons'>email</i> <b>amranidev@gmail.com</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Github</h5>
+                <ul>
+                  <iframe src="https://ghbtns.com/github-btn.html?user=amranidev&repo=ajaxis&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2015 
+            <a class="grey-text text-lighten-4 right" href="#!">MIT License</a>
+            </div>
+          </div>
+        </footer>
