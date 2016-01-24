9d88ce20ad89d6a82d7b6e624dbe01a08cc401af
Amrani <amranidev@gmail.com>
2016-01-24 22:43:02 +0000

    fix compatibility bootstrap

--- AjaxisTest/resources/views/ContactBootstrap.blade.php
+++ AjaxisTest/resources/views/ContactBootstrap.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.MasterBt')
 @section('title','Contact Bootstrap')
 @section('content')
-<button data-toggle="modal" data-target="#myModal" class = 'create btn btn-primary' data-link = '/ContactBt/create/'><i class = 'material-icons'>add</i></button>
+<button data-toggle="modal" data-target="#myModal" class = 'create btn btn-primary' data-link = '/ContactBt/create'><i class = 'material-icons'>add</i></button>
 <table class = 'table table-hover'>
     <thead>
         <th>First Name</th>
@@ -19,9 +19,9 @@
             <td>{{$test->phone}}</td>
             <td>
                 <div class = 'row'>
-                    <button data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = '/ContactBt/{{$test->id}}/delete/'><i class = 'Small material-icons'>delete</i></button>
-                    <button data-toggle="modal" data-target="#myModal" class = 'edit btn btn-success btn-xs'  data-link = '/ContactBt/{{$test->id}}/edit/'><i class = 'material-icons'>mode_edit</i></button>
-                    <button data-toggle="modal" data-target="#myModal" class = 'display btn btn-primary btn-xs'  data-link = '/ContactBt/{{$test->id}}/show/'><i class = 'material-icons'>info</i></button>
+                    <button data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = '/ContactBt/{{$test->id}}/delete'><i class = 'Small material-icons'>delete</i></button>
+                    <button data-toggle="modal" data-target="#myModal" class = 'edit btn btn-success btn-xs'  data-link = '/ContactBt/{{$test->id}}/edit'><i class = 'material-icons'>mode_edit</i></button>
+                    <button data-toggle="modal" data-target="#myModal" class = 'display btn btn-primary btn-xs'  data-link = '/ContactBt/{{$test->id}}/show'><i class = 'material-icons'>info</i></button>
                 </div>
             </td>
         </tr>
